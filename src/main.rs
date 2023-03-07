50d87ad89627c6d7f98b5d4161ff7b34201faedc
Quentin <q.barthelemy@student.helmo.be>
2023-03-07 17:43:56 +0100

    using json values for multicast

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 mod models;
 mod infrastructure;
 
-use std::net::{TcpListener, TcpStream, UdpSocket, ToSocketAddrs};
+use std::net::{TcpStream, UdpSocket};
 use std::thread;
 use std::str;
 use std::io::{self, Read, Write};
 use regex::Regex;
-use std::path::Path;
-use std::fs::File;
 use crate::infrastructure::json_repository::get_relay;
-use crate::models::{Domains, Relay};
+use crate::models::{Clients, Domains, Relay};
 
 fn handle_client(mut stream: TcpStream) {
     println!("New client: {}", stream.peer_addr().unwrap());
@@ -52,25 +50,18 @@ fn handle_client(mut stream: TcpStream) {
 
 fn main() -> io::Result<()> {
 
-    /*let path = Path::new("./data/relay.json");
-    let mut file  = File::open(path).expect("File not found");
-    let mut contents = String::new();
-    File::read_to_string(&mut file, &mut contents).expect("Something went wrong reading the file");
-    let mut relay : Relay = serde_json::from_str(&contents).expect("JSON parsing error");
-    println!("{} {} {} {}", &relay.get_multicast_address(), &relay.get_multicast_port(), &relay.get_network_interface(), &relay.get_network_interface());
-    */
-    let connected_domains = new:Vec<>();
+
+    let connected_domains: Vec<Clients> = Vec::new();
+
     let relay = get_relay();
-    println!("Relay {} {} {} {}", relay.get_multicast_address().clone(), relay.get_multicast_port().clone(), relay.get_network_interface().clone(), relay.get_network_interface().clone());
+    println!("Relay {}", relay.to_string());
 
 
     // Start UDP multicast listener on address 224.1.1.255:23845
-    let multicast_socket = UdpSocket::bind("0.0.0.0:23502")?;
-    let multicast_addr:std::net::SocketAddrV4 = "224.1.1.255:23502".parse().unwrap();
+    let multicast_socket = UdpSocket::bind(format!("0.0.0.0:{}", relay.get_multicast_port()))?;
+    let multicast_addr:std::net::SocketAddrV4 = format!("{}:{}", relay.get_multicast_address(), relay.get_multicast_port()).parse().unwrap();
     multicast_socket.join_multicast_v4(&multicast_addr.ip(), &"0.0.0.0".parse().unwrap())?;
 
-    // Start TCP server on 127.0.0.1:23847
-    //let tcp_listener = TcpListener::bind("127.0.0.1:23502")?;
 
     // Spawn thread to handle multicast messages
     thread::spawn(move || {
@@ -81,6 +72,7 @@ fn main() -> io::Result<()> {
                 Ok((size, src)) => {
                     let msg = str::from_utf8(&buf[..size]).unwrap();
                     println!("Received multicast message from {}: {}", src, msg);
+
                     // Parse port and domain from multicast message using regex
                     match regex.captures(msg) {
                         Some(captures) => {
@@ -115,19 +107,6 @@ fn main() -> io::Result<()> {
             }
         }
     });
-
-    /* Wait for incoming TCP connections
-    for stream in tcp_listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New client: {}", stream.peer_addr().unwrap());
-                // TODO: Add client to some kind of client list and handle communication
-            }
-            Err(e) => {
-                println!("Error accepting client: {}", e);
-            }
-        }
-    }*/
 
     Ok(())
 }
